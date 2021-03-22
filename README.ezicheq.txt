003db876ebe68ff89eb17da61c4daf6912be78c7
Mike Dilger <mike@ezicheq.com>
2021-03-23 09:07:08 +1300

    Custom build for Ezicheq
    use HUSKY_SKIP_HOOKS=1 to commit into this repo w/o their tools complaining

---
+++ README.ezicheq.txt
@@ -0,0 +1,13 @@
+See https://ckeditor.com/docs/ckeditor5/latest/builds/guides/development/custom-builds.html
+
+In short:
+
+1) Edit packages/ckeditor5-build-ezicheq/src/ckeditor.js
+
+2) Run (from our directory)
+   $ yarn run build
+
+3) Test it by browsing to sample/index.html
+
+3) Pick up your output in build/ckeditor.js
+
---
+++ packages/ckeditor5-build-ezicheq/src/ckeditor.js
@@ -0,0 +1,88 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+// The editor creator to use.
+import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
+import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
+import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+
+export default class EzicheqEditor extends ClassicEditorBase {}
+
+// Plugins to include in the build.
+EzicheqEditor.builtinPlugins = [
+    Essentials,
+    Autoformat,
+    Bold,
+    Italic,
+    Underline,
+    Strikethrough,
+    Superscript,
+    Subscript,
+    Link,
+    HorizontalLine,
+    SpecialCharacters,
+    SpecialCharactersEssentials,
+    Heading,
+    List,
+    Paragraph,
+    BlockQuote,
+    Indent,
+    IndentBlock,
+    PasteFromOffice,
+    Clipboard,
+    TextTransformation,
+    Alignment
+];
+
+// Editor configuration.
+EzicheqEditor.defaultConfig = {
+    toolbar: {
+	items: [
+	    'undo',
+	    'redo',
+	    '|',
+	    'bold',
+	    'italic',
+            'underline',
+	    'heading',
+	    '|',
+	    'bulletedList',
+	    'numberedList',
+	    'blockQuote',
+	    'outdent',
+	    'indent',
+            'alignment',
+	    '|',
+	    'link',
+            'horizontalLine',
+            'specialCharacters',
+            'superscript',
+            'subscript',
+            'strikethrough',
+	]
+    },
+    // This value must be kept in sync with the language defined in webpack.config.js.
+    language: 'en'
+};
