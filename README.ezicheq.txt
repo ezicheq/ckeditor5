9f8693d4ef5e833f1c722f48161f0c2f5ce47fa9
Mike Dilger <mike@ezicheq.com>
2021-04-08 09:42:53 +1200

    Apply ezicheq changes and provide a README file for us

---
+++ README.ezicheq.txt
@@ -0,0 +1,15 @@
+See https://ckeditor.com/docs/ckeditor5/latest/builds/guides/development/custom-builds.html
+
+In short:
+
+1) Edit packages/ckeditor5-build-ezicheq/src/ckeditor.js
+
+2) Run (from our directory)
+   $ yarn run build
+
+3) Test it by browsing to sample/index.html
+
+3) Pick up your output in build/ckeditor.js
+
+
+To git commit without complaints, set HUSKY_SKIP_HOOKS=1
--- packages/ckeditor5-build-ezicheq/src/ckeditor.js
+++ packages/ckeditor5-build-ezicheq/src/ckeditor.js
@@ -4,101 +4,85 @@
  */
 
 // The editor creator to use.
-import EzicheqEditorBase from '@ckeditor/ckeditor5-editor-ezicheq/src/ezicheqeditor';
-
+import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
 import Link from '@ckeditor/ckeditor5-link/src/link';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
+import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
+import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 
-export default class EzicheqEditor extends EzicheqEditorBase {}
+export default class EzicheqEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 EzicheqEditor.builtinPlugins = [
-	Essentials,
-	UploadAdapter,
-	Autoformat,
-	Bold,
-	Italic,
-	BlockQuote,
-	CKFinder,
-	CloudServices,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
-	Link,
-	List,
-	MediaEmbed,
-	Paragraph,
-	PasteFromOffice,
-	Table,
-	TableToolbar,
-	TextTransformation
+    Essentials,
+    Autoformat,
+    Bold,
+    Italic,
+    Underline,
+    Strikethrough,
+    Superscript,
+    Subscript,
+    Link,
+    HorizontalLine,
+    SpecialCharacters,
+    SpecialCharactersEssentials,
+    Heading,
+    List,
+    Paragraph,
+    BlockQuote,
+    Indent,
+    IndentBlock,
+    PasteFromOffice,
+    Clipboard,
+    TextTransformation,
+    Alignment
 ];
 
 // Editor configuration.
 EzicheqEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'outdent',
-			'indent',
-			'|',
-			'uploadImage',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+    toolbar: {
+	items: [
+	    'undo',
+	    'redo',
+	    '|',
+	    'bold',
+	    'italic',
+            'underline',
+	    'heading',
+	    '|',
+	    'bulletedList',
+	    'numberedList',
+	    'blockQuote',
+	    'outdent',
+	    'indent',
+            'alignment',
+	    '|',
+	    'link',
+            'horizontalLine',
+            'specialCharacters',
+            'superscript',
+            'subscript',
+            'strikethrough',
+	]
+    },
+    // This value must be kept in sync with the language defined in webpack.config.js.
+    language: 'en'
 };
